a01eab73f8fb16c90e742406d158dc4affd259f6
Andrés Largo <felipeflogxd@gmail.com>
2020-08-19 00:44:28 -0500

    Drawer settings

---
+++ app/src/main/java/com/unilibre/familiaapp/AuthActivity.java
@@ -0,0 +1,14 @@
+package com.unilibre.familiaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
