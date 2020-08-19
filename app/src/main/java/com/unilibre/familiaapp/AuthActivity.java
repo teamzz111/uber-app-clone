d8cccdbfb6d50cf55312751d1782cde714794a6e
Andrés Largo <felipeflogxd@gmail.com>
2020-08-19 00:45:00 -0500

    Checking logged or not

--- app/src/main/java/com/unilibre/familiaapp/AuthActivity.java
+++ app/src/main/java/com/unilibre/familiaapp/AuthActivity.java
@@ -1,14 +1,30 @@
 package com.unilibre.familiaapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.unilibre.familiaapp.ui.login.LoginActivity;
+
 public class AuthActivity extends AppCompatActivity {
+
+    private FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        fAuth = FirebaseAuth.getInstance();
+    }
+
+    protected void onStart(){
+        super.onStart();
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(AuthActivity.this, HomeActivity.class));
+        } else {
+            startActivity(new Intent(AuthActivity.this, LoginActivity.class));
+        }
     }
 }
