943ef4afed573bb8991b4764712f29e31b9e2c3b
Erika Infante <einfante@dbsolution.com.co>
2020-09-08 18:24:03 -0500

    Radio

--- app/src/main/java/com/unilibre/familiaapp/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/unilibre/familiaapp/ui/gallery/GalleryFragment.java
@@ -8,6 +8,9 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.RadioGroup;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -16,23 +19,39 @@ import com.google.android.material.snackbar.Snackbar;
 import com.unilibre.familiaapp.R;
 
 public class GalleryFragment extends Fragment {
-
+    private View root;
     private GalleryViewModel galleryViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        final View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        root = inflater.inflate(R.layout.fragment_gallery, container, false);
         final Button check = root.findViewById(R.id.button);
         final CheckBox chebkbox1 = root.findViewById(R.id.check1);
         final CheckBox chebkbox2 = root.findViewById(R.id.check2);
         final Spinner spinner = root.findViewById(R.id.planets_spinner);
         final SeekBar seek = root.findViewById(R.id.seekBar);
+
+
         check.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Snackbar.make(root, "Uno: " + chebkbox1.isChecked() + " Dos: "+ chebkbox2.isChecked() + " S: "+ spinner.getSelectedItem().toString() + "SEEK: " + seek.getProgress(), Snackbar.LENGTH_LONG)
+                addListenerOnButton();
+                Snackbar.make(root, "Uno: " + chebkbox1.isChecked() + " Dos: "+ chebkbox2.isChecked() + " S: "+ spinner.getSelectedItem().toString()+ "SEEK: " + seek.getProgress(), Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
         return root;
+    }
+    public void addListenerOnButton() {
+        RadioGroup radioGroup = root.findViewById(R.id.radio);
+
+
+                // get selected radio button from radioGroup
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+
+                // find the radiobutton by returned id
+                RadioButton radioButton = root.findViewById(selectedId);
+
+                Toast.makeText(getActivity(), radioButton.getText(), Toast.LENGTH_SHORT).show();
+
     }
 }
