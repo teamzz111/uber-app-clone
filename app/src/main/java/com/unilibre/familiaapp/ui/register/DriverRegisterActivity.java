68f2af8bb5cd9e585c9da4c8e0969232da442051
Jorge Morales <jamorales516@icloud.com>
2020-10-13 12:46:54 -0500

    Buttons added and getting ready to upload files

--- app/src/main/java/com/unilibre/familiaapp/ui/register/DriverRegisterActivity.java
+++ app/src/main/java/com/unilibre/familiaapp/ui/register/DriverRegisterActivity.java
@@ -2,15 +2,35 @@ package com.unilibre.familiaapp.ui.register;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.unilibre.familiaapp.R;
 
-public class DriverRegisterActivity extends AppCompatActivity {
+public class DriverRegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int PICK_IMAGE_REQUEST = 234;
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_register);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+    }
+
+    private void FileChooser(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select a image"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
