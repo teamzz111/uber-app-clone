6e296d38358415fd2dd0a5630c9aaf155f3146b0
Jorge Morales <jamorales516@icloud.com>
2020-10-13 14:42:24 -0500

    Getting documents from user phone

--- app/src/main/java/com/unilibre/familiaapp/ui/register/DriverRegisterActivity.java
+++ app/src/main/java/com/unilibre/familiaapp/ui/register/DriverRegisterActivity.java
@@ -3,9 +3,11 @@ package com.unilibre.familiaapp.ui.register;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -14,7 +16,8 @@ import com.unilibre.familiaapp.R;
 public class DriverRegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
     private StorageReference mStorageRef;
-    Button property_card, soat, mechanical_inspection;
+    private Button property_card, soat, mechanical_inspection;
+    private Uri property_card_path, soat_path, mechanical_inspection_path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,28 +25,55 @@ public class DriverRegisterActivity extends AppCompatActivity implements View.On
         setContentView(R.layout.activity_driver_register);
         mStorageRef = FirebaseStorage.getInstance().getReference();
 
-        property_card = findViewById(R.id.propertycard);
-        soat = findViewById(R.id.soat);
-        mechanical_inspection = findViewById(R.id.mechanical);
+        property_card = (Button) findViewById(R.id.propertycard);
+        soat = (Button)findViewById(R.id.soat);
+        mechanical_inspection = (Button)findViewById(R.id.mechanical);
+
+        property_card.setOnClickListener(this);
+        soat.setOnClickListener(this);
+        mechanical_inspection.setOnClickListener(this);
     }
 
     private void FileChooser(int REQUEST_CODE){
         Intent intent = new Intent();
         intent.setType("*/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select a image"), REQUEST_CODE);
+        startActivityForResult(Intent.createChooser(intent, "Select a file"), REQUEST_CODE);
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //String Fpath = data.getDataString();
+        // do somthing...
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && data != null){
+            if (requestCode == 234){
+                property_card_path = data.getData();
+            }
+            else if (requestCode == 537){
+                soat_path = data.getData();
+            }
+            else if (requestCode == 451){
+                mechanical_inspection_path = data.getData();
+            }
+            //Toast.makeText(DriverRegisterActivity.this, "Path: "+data.getData()+", RqCode: "+requestCode+", RsCode: "+resultCode, Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     public void onClick(View view) {
         if(view == property_card){
             // Do smt
+            FileChooser(234);
+            //Toast.makeText(DriverRegisterActivity.this, "Property Card", Toast.LENGTH_SHORT).show();
         }
-        if(view==soat){
+        else if(view==soat){
             // Do smt
+            FileChooser(537);
+            //Toast.makeText(DriverRegisterActivity.this, "SOAT", Toast.LENGTH_SHORT).show();
         }
-        if (view==mechanical_inspection){
+        else if (view==mechanical_inspection){
             // Do smt
+            FileChooser(451);
+            //Toast.makeText(DriverRegisterActivity.this, "Mechanical Inspection", Toast.LENGTH_SHORT).show();
         }
     }
 }
