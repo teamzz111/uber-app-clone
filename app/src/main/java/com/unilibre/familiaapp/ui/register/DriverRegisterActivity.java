c82cd21db6601f5e7574235900eca855a98c123f
Jorge Morales <jamorales516@icloud.com>
2020-10-12 21:47:31 -0500

    Activity for driver registration added

---
+++ app/src/main/java/com/unilibre/familiaapp/ui/register/DriverRegisterActivity.java
@@ -0,0 +1,16 @@
+package com.unilibre.familiaapp.ui.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.unilibre.familiaapp.R;
+
+public class DriverRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_register);
+    }
+}
